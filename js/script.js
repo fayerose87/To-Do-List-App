2b01cf47842ea15aa936b5436cf354e6f0224787
fayerose87 <79806977+fayerose87@users.noreply.github.com>
2021-04-04 19:40:43 -0500

    Updated vanilla JS to javascript

--- js/script.js
+++ js/script.js
@@ -1,38 +1,43 @@
+// Add code so that list item is added when return is pressed
+$('#input').keypress(function(e) {
+  if (e.keyCode === 13) {
+    e.preventDefault();
+    newItem();
+  }
+});
 
 function newItem(){
 
-  //javascript
-  //1. Adding a new item to the list of items: 
-     let li = document.createElement("li");
-     let inputValue = document.getElementById("input").value;
-     let text = document.createTextNode(inputValue);
-     li.appendChild(text);
-  
+ // Add a new item to the list of items: 
+    let li = $('<li></li>');
+    let inputValue = $('#input').val();
+    li.append(inputValue);
+
      if (inputValue === '') {
        alert("You must write something!");
      } else {
-       let list = document.querySelector('#list');
-       list.appendChild(li);
+       $('#list').append(li);
      }
   
-   //2. Crossing out an item from the list of items:
-     function crossOut() {
-       li.classList.toggle("strike");
-     }
+  // Cross out an item from the list of items:'
+   function crossOut() {
+    li.toggleClass('strike');
+  }
+    li.on('dblclick', crossOut);
+      
   
-     li.addEventListener("dblclick",crossOut);
+  // Add the delete button "X": 
+     let crossOutButton = $('<crossOutButton></crossOutButton>');
+     crossOutButton.append(document.createTextNode('X'));
+     li.append(crossOutButton);
+
+     crossOutButton.on('click', deleteListItem);
   
-   //3(i). Adding the delete button "X": 
-     let crossOutButton = document.createElement("crossOutButton");
-     crossOutButton.appendChild(document.createTextNode("X"));
-     li.appendChild(crossOutButton);
-  
-     crossOutButton.addEventListener("click", deleteListItem);
-   //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
-     function deleteListItem(){
-       li.classList.add("delete")
-     }
-   // 4. Reordering the items: 
+  // Add CLASS DELETE (DISPLAY: NONE) from the css:
+    function deleteListItem () {
+     li.addClass('delete')
+    }
+
+   // Reorder the items: 
      $('#list').sortable();
-  
   }
